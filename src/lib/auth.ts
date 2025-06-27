01015e9e8a5d09fff34d9c967116e73e28632489
Tenma <tenma@osxt.com>
2025-06-27 09:54:35 +0200

    fix url

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,7 +10,7 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      redirectURI: `http://localhost:3000/api/auth/callback/google`,
+      redirectURI: `${process.env.SERVER_URL}/api/auth/callback/google`,
     },
   },
 });
