6fd456e714cc564fe97f4b59860cf0b5a906e7aa
Tenma <tenma@osxt.com>
2025-06-27 09:48:27 +0200

    fix import

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "./database";
+import { prisma } from "./database.js";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
